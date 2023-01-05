6e0e01b5de2473e442c93b370eb69e12059b1959
Pon <pon.m22@gmail.com>
2023-01-05 22:05:16 +0700

    CRUD

--- index.js
+++ index.js
@@ -18,13 +18,14 @@ app.use(express.urlencoded({
 mongoose.Promise = global.Promise;
 mongoose.connect(dbConfig.url)
     .then(() =>{
-        Customer.deleteMany({}, (err) => {
-    if(err){
-        process.exit();
-        }
-        console.log('Remove Collection of Customer')
-        initCustomer();
-        });
+    //     Customer.deleteMany({}, (err) => {
+    // if(err){
+    //     process.exit();
+    //     }
+    //     console.log('Remove Collection of Customer')
+    //     initCustomer();
+    //     });
+        console.log('Connect to MongoDB')
     }).catch(err=>{
         console.log('Cannot Connect to MongoDB')
         process.exit();
--- public/routes/customer.route.js
+++ public/routes/customer.route.js
@@ -1,5 +1,9 @@
 module.exports = (app) =>{
     const customer = require("../controllers/customer.controller.js")
     app.get('/', customer.index);
-    app.get('/api/customer', customer.findAll);
+    app.post('/api/customer', customer.create)
+    app.get('/api/customer', customer.findAll)
+    app.get('/api/customer/:customerId', customer.findById)
+    app.put('/api/customer/:customerId', customer.update)
+    app.delete('/api/customer/:customerId', customer.delete)
 }
